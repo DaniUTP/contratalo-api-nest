b0f907696374f93e7cfb3317d3386f0faaf819b6
DaniUTP <144512666+DaniUTP@users.noreply.github.com>
2025-09-17 20:24:07 -0500

    Update meOk.dto.ts

--- src/modules/auth/dto/response/me/meOk.dto.ts
+++ src/modules/auth/dto/response/me/meOk.dto.ts
@@ -7,8 +7,18 @@ export class MeOKDto {
     })
     name: string;
     @ApiProperty({
+        description: 'Apellido del usuario',
+        example: 'Aldana',
+    })
+    last_name:string;
+    @ApiProperty({
         description: 'Email del usuario',
         example: 'aldanagerardo24@gmail.com',
     })
     email: string;
+    @ApiProperty({
+        description: 'Telefono del usuario',
+        example: 123456789,
+    })
+    phone:number;
 }
